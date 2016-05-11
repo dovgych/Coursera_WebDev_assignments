4294d5850f95a68f56e187185592dc9bb03d1d81
dovgych <g.dovgych@gmail.com>
2016-05-11 23:36:53 +0300

    Solution to the Module 4 Assignment

---
+++ mod4_assignment/start_coding.bat
@@ -0,0 +1,6 @@
+@Open new Sublime Text 3 Workspace in the current folder
+subl.exe .
+
+@Synchronizes current dir and all in-folders with the browser
+browser-sync start --server --directory --files "**/*"
+
